e31327b589784bf4106519e4bf29b8f84056f45c
Pedro Guidotte <peguidotte@gmail.com>
2025-02-18 13:05:40 -0300

    Primeira Aula
    Aprendi a utilizar melhor a integração de Fastify com o Zod, usando swagger para automatizar a documentação das API's criadas.

---
+++ src/routes/subscribe-to-event-route.ts
@@ -0,0 +1,35 @@
+import { z } from 'zod'
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+
+export const SubscribeToEventRoute: FastifyPluginAsyncZod = async app => {
+  app.post(
+    '/subscriptions',
+    {
+      schema: {
+        summary: 'Subscribe someone to the event',
+        tags: ['subscription'],
+
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+        }),
+        response: {
+          201: z.object({
+            name: z.string(),
+            email: z.string(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { name, email } = request.body
+
+      //inscrição no banco de dados aqui
+
+      return reply.status(201).send({
+        name,
+        email,
+      })
+    }
+  )
+}
--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,40 @@
-import { fastify } from 'fastify';
+import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
+import {
+  validatorCompiler,
+  serializerCompiler,
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+} from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { SubscribeToEventRoute } from './routes/subscribe-to-event-route'
+import { env } from './env'
 
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'NLW Connect',
+      version: '0.0.1',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(SubscribeToEventRoute)
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log('HTTP server running')
+})
